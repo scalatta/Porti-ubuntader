ccff2820bc0e031ccfb0e3ac41b229e64820976f
Attila <79305182+scalatta@users.noreply.github.com>
2024-09-01 00:33:55 +0200

    Update big-upgrade.sh

--- big-upgrade.sh
+++ big-upgrade.sh
@@ -8,7 +8,7 @@ LOG_FILE="/var/log/portainer_update.log"
 
 # Function to log messages
 log_message() {
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
+    echo "[LOG] $(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
 }
 
 # Start of the script
